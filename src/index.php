7fc34a847204f0e051e1a492c7deb7e0b984ec0c
saurabh singh <47899416+saurabhs1998@users.noreply.github.com>
2020-05-08 13:25:41 +0530

    updated

--- src/index.php
+++ src/index.php
@@ -374,6 +374,7 @@
             </div>
         </div>
     </footer>
+    
   
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
